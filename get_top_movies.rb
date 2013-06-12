94a6aa85058e4fe873e143c61678ffbdf3334f06
ali aamir <ali_aamir@hotmail.com>
2013-06-12 18:11:20 -0500

    Completed 'get_top_movies' except for deleting number of reviews part within the data

--- get_top_movies.rb
+++ get_top_movies.rb
@@ -10,8 +10,10 @@ page = Nokogiri::HTML(open("http://www.rottentomatoes.com/top/bestofrt/?year=201
 
 movies = page.css('.content_body.clearfix > table.left.rt_table tr')
 
-
 movies.each do |movie|
-	movie = movie.txt.text.gsub(/[\n\t]/,'')
-	m.write(movie)
+	movie = movie.text.gsub(/[\n\t]/,'').gsub('(2013)', '')
+	
+	m.write(movie + "reviews\n") if !movie.nil? && movie != "Rank" && movie != "Tomatometer" && movie != "Title" && "No.of Reviews"
+		end
+	end
 
