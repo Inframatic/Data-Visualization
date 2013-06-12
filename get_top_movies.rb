965b59b665a916230e3f2f988a2b86a3b783717f
ali aamir <ali_aamir@hotmail.com>
2013-06-12 17:33:45 -0500

    Worked on 'get_top_movies.rb' file

--- get_top_movies.rb
+++ get_top_movies.rb
@@ -2,4 +2,16 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-File.open('games.txt', 'w') do |f|
+
+File.open('movies .txt', 'w') do |m|
+
+
+page = Nokogiri::HTML(open("http://www.rottentomatoes.com/top/bestofrt/?year=2013"))
+
+movies = page.css('.content_body.clearfix > table.left.rt_table tr')
+
+
+movies.each do |movie|
+	movie = movie.txt.text.gsub(/[\n\t]/,'')
+	m.write(movie)
+
