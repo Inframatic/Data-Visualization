fdf418b55ee54adc5b115f0815787bc93731802c
ali aamir <ali_aamir@hotmail.com>
2013-06-13 18:30:36 -0500

    Completed divs_for_top_movie.html and created div tags

--- get_divs_for_top_movies.rb
+++ get_divs_for_top_movies.rb
@@ -3,19 +3,32 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
-header = ['title', 'rating']
+File.open('divs_for_top_movie.html', 'w') do |html|
 
-CSV.foreach('movies.csv') do |row|
+	movie_array = []
 
-	header.each_index do |i| 
-		if header[i] != row[i]
-			puts "#{header[i]} is #{row[i]}"
-		end
+	CSV.foreach('movies.csv') do |row|
+
+		title = row[0]
+		rating = row[1]
+		all = [title, rating]
+		movie_array << all
+
+		# row.each do |i| 
+		# 	title = i.read
+		# 	rating = i.read
+		# 	all = [title, rating]
+		# movie_array << all
+    	
+    end
+
+
+	movie_array.each do |row|
+		
+ 	  div_height = (row[0]).to_s
+    html.write("<div class='bar_wrapper' data-name='" + row[0] + "'>\n")
+    html.write("  <div class='bar' style='height:" + div_height + "%;'></div>\n")
+    html.write("</div>\n")
+
 	end
 end
-
-
-	# for i in 0..100 do
-	# 	div_height = (100-())
-
-
--- get_top_movies.rb
+++ get_top_movies.rb
@@ -4,9 +4,6 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
-
-
-
 page = Nokogiri::HTML(open("http://www.rottentomatoes.com/top/bestofrt/?year=2013"))
 
 movies = page.css('.content_body.clearfix > table.left.rt_table tr')
@@ -19,9 +16,9 @@ movies.each do |row|
 	# movie.each do |a|
 		movie =row.css('td')
 		title = movie[2].text.strip.gsub('(2013)', '') if !movie[2].nil? && !movie[2].text.empty?
-		rating = movie[1].text.strip.to_i if !movie[1].nil?
+		rating = movie[1].text.strip.to_i if !movie[1].nil? && !movie[1].text.empty?
 		 data = [title, rating]
-		 movie_array << data
+		 movie_array << data if !title.nil? || !rating.nil?
 end
 
 CSV.open('movies.csv', 'wb') do |csvrow|
