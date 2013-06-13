b09a9a3e02ede7c529a3c8ea6c9b3278385077e0
ali aamir <ali_aamir@hotmail.com>
2013-06-12 20:21:43 -0500

    Added 'get_divs_for_top_movies.rb' file

--- get_top_movies.rb
+++ get_top_movies.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 
-File.open('movies .txt', 'w') do |m|
+File.open('movies.txt', 'w') do |m|
 
 
 page = Nokogiri::HTML(open("http://www.rottentomatoes.com/top/bestofrt/?year=2013"))
@@ -11,9 +11,9 @@ page = Nokogiri::HTML(open("http://www.rottentomatoes.com/top/bestofrt/?year=201
 movies = page.css('.content_body.clearfix > table.left.rt_table tr')
 
 movies.each do |movie|
-	movie = movie.text.gsub(/[\n\t]/,'').gsub('(2013)', '')
+	movie = movie.text.gsub(/[\n\t]/,'').gsub('(2013)', '').downcase
 	
-	m.write(movie + "reviews\n") if !movie.nil? && movie != "Rank" && movie != "Tomatometer" && movie != "Title" && "No.of Reviews"
+	m.write(movie + "\n") if !movie.nil?  # movie != "Rank" && movie != "Tomatometer" && movie != "Title" && "No.of Reviews"
 		end
 	end
 
